fcd593ef2d1ecc80ca6b6238b7c9fae844011982
Diana.Lozano <Diana.Lozano@ibm.com>
2016-09-27 11:56:49 -0500

    flickr stuff

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
+import creds from './creds.js';
+
 
 export default class Gallery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+  }
+
+  buildPhotoUrl(photo) {
+    return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server +
+    '/' + photo.id + '_' + photo.secret + '.jpg';
+  }
+
+  componentDidMount() {
+    var base = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=";
+    var extra = "&tags=black+and+white&safe_search=true&is_getty=true&format=json&nojsoncallback=1"
+    console.log("hello")
+    fetch(base+creds.apikey+extra)
+      .then((response) => response.json())
+      .then((response) => {
+        var firstTen = response.photos.photo.slice(0, 10);
+        var imageUrls = firstTen.map(photo => this.buildPhotoUrl(photo))
+        this.setState({ photos: imageUrls })
+        console.log(this.state.photos)
+      })
+  }
+
   render() {
+    if (this.state.photos) {
+      var list = this.state.photos.map(function(photo, index) {
+        return <img src={photo} />
+      })
+    }
+
     return (
       <div>
         <h1 className="section-name">Gallery</h1>
         <ul className="gallery">
-          <li>
-            <div className="gallery-img-div">
-              <div><img className="gallery-img" src="http://media.gettyimages.com/photos/double-exposure-portrait-of-a-young-man-and-a-tree-picture-id562609891?s=170667a" /></div>
-              <p>caption 1</p>
-              <p>caption 2</p>
-            </div>
-          </li>
-          <li>
-            <div className="gallery-img-div">
-              <div><img className="gallery-img" src="http://media.gettyimages.com/photos/girl-covering-her-mouth-picture-id142005745?s=170667a" /></div>
-              <p>caption 1</p>
-              <p>caption 2</p>
-            </div>
-          </li>
+          {list}
         </ul>
       </div>
     )
